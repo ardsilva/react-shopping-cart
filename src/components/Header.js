b231a0425bedf3d3e2129f4b1e8d5188e2a77e1e
Alexandre Ribeiro da Silva <alexandre.ribeiro@symphony.com>
2019-02-19 14:18:11 -0300

    added finalprice in shopping cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -71,25 +71,34 @@ class Header extends Component {
     let cartItems;
     cartItems = this.state.cart.map(product => {
       return (
-        <li className="cart-item" key={product.name}>
-          <div className="product-info">
-            <p className="product-name">{product.name}</p>
-            <p className="product-price">{product.price}</p>
+        <div>
+          <li className="cart-item" key={product.name}>
+            <div className="product-info">
+              <p className="product-name">{product.name}</p>
+              <p className="product-price">{product.price}</p>
+            </div>
+            <div className="product-total">
+              <p className="quantity">
+                {product.quantity} {product.quantity > 1 ? "Qtdes" : "Qtd"}{" "}
+              </p>
+              <p className="amount">{product.quantity * product.price}</p>
+            </div>
+            <a
+              className="product-remove"
+              href="#"
+              onClick={this.props.removeProduct.bind(this, product.id)}
+            >
+              ×
+            </a>
+          </li>
+          <div className="totalItems">
+            {this.props.total ? (
+              <p className="total-amount-itens">Valor Total = R${this.props.total}</p>
+            ) : (
+              ""
+            )}
           </div>
-          <div className="product-total">
-            <p className="quantity">
-              {product.quantity} {product.quantity > 1 ? "Qtdes" : "Qtd"}{" "}
-            </p>
-            <p className="amount">{product.quantity * product.price}</p>
-          </div>
-          <a
-            className="product-remove"
-            href="#"
-            onClick={this.props.removeProduct.bind(this, product.id)}
-          >
-            ×
-          </a>
-        </li>
+        </div>
       );
     });
     let view;
@@ -203,13 +212,6 @@ class Header extends Component {
               ref="cartPreview"
             >
               <CartScrollBar>{view}</CartScrollBar>
-              <div className="totalItems">
-              {this.props.total ? (
-                <p className="amount">{this.props.total}</p>
-              ) : (
-                ""
-              )}
-              </div>
               <div className="action-block">
                 <button
                   type="button"
